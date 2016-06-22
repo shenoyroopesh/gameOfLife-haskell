3fd489e9248bbf966df8c252f478f450ea7a9068
Roopesh Shenoy <shenoy.roopesh@gmail.com>
2016-06-23 03:03:25 +0530

    adding Signatures to the functions

--- gameOfLife.hs
+++ gameOfLife.hs
@@ -1,24 +1,32 @@
 import Data.Map
+import Data.Lens
 
 data Cell = Cell { x :: Int, y :: Int, alive :: Bool, aliveInNextTick :: Bool, neighbouringCells :: [Cell] }
 
+tick :: [Cell] -> [Cell]
 tick = (\xs = map (fillNeighbours xs) xs) |> map (setL aliveInNextTick True) |>
 				map (killIfNeighbours (<2)) |> map (killIfNeighbours (>3)) |> birthNewCells |> (map finalize)
 
+fillNeighbours :: [Cell] -> Cell -> Cell
 fillNeighbours xs cell@(Cell x y _ _ _) = 
 	setL neighbouringCells (map cellFromPosition xs (allCombinations [x-1 .. x+1] [y-1 .. y+1])) cell
 
 -- if find cell in list then give it back, else create new cell
+cellFromPosition :: [Cell] -> (Int, Int) -> Cell
 cellFromPosition xs (x,y) = maybe (Cell x y False False []) id (find (\cell -> getL x cell == x && getL y cell == y) xs)
 
+killIfNeighbours :: (a -> Bool) -> Cell -> Cell
 killIfNeighbours condition cell@(Cell _ _ _ _ neighbouringCells) = 
 	if (condition (length (filter alive neighbouringCells)) then (setL aliveInNextTick False cell) else cell
 
 -- first collect all neighbouring cells in one place - then make individual counts of each of them - then birth the ones that were dead
+birthNewCells :: [Cell] -> [Cell]
 birthNewCells a = nub a:(getNewBirthCells a)
 
+getNewBirthCells :: [Cell] -> [Cell]
 getNewBirthCells = (map neighbouringCells) |> concat |> uniquesWithCounts |> (filter (count |> >= 3)) |> map (setL aliveInNextTick true)
 
+finalize :: Cell -> Cell
 finalize x = setL alive (getL aliveInNextTick x) x
 
 
