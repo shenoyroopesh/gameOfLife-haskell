8ef709c2b0b2c335475c1e780008a3921548374c
Roopesh Shenoy <shenoy.roopesh@gmail.com>
2016-06-23 05:03:25 +0530

    Some more compile issues fixed

--- gameOfLife.hs
+++ gameOfLife.hs
@@ -1,43 +1,49 @@
 {-# OPTIONS_GHC -XTupleSections #-}
+{-# LANGUAGE ScopedTypeVariables, TemplateHaskell #-}
 
 import Test.QuickCheck
 import Data.List
-import Data.Lens.Common
+import Control.Lens
 import qualified Data.Map as Map
 
-data Cell = Cell { x :: Int, y :: Int, alive :: Bool, aliveInNextTick :: Bool, neighbouringCells :: [Cell] }
+data Cell = Cell { _x :: Int, _y :: Int, _alive :: Bool, _aliveInNextTick :: Bool, _neighbouringCells :: [Cell] }
+
+makeLenses ''Cell
 
 tick :: [Cell] -> [Cell]
-tick = (\xs -> map (fillNeighbours xs) xs) |> map (setL aliveInNextTick True) |>
+tick = (\xs -> map (fillNeighbours xs) xs) |> map (set aliveInNextTick True) |>
 				map (killIfNeighbours (<2)) |> map (killIfNeighbours (>3)) |> birthNewCells |> (map finalize)
 
 fillNeighbours :: [Cell] -> Cell -> Cell
 fillNeighbours xs cell@(Cell x y _ _ _) = 
-	setL neighbouringCells (map cellFromPosition xs (allCombinations [x-1 .. x+1] [y-1 .. y+1])) cell
+	set neighbouringCells (map (cellFromPosition xs) (neighbouringPosns x y)) cell
+
+neighbouringPosns :: Int -> Int -> [(Int, Int)]	
+neighbouringPosns x y = allCombinations [(x-1) .. (x+1)] [(y-1) .. (y+1)]
 
 -- if find cell in list then give it back, else create new cell
 cellFromPosition :: [Cell] -> (Int, Int) -> Cell
-cellFromPosition xs (x,y) = maybe (Cell x y False False []) id (find (\cell -> getL x cell == x && getL y cell == y) xs)
+cellFromPosition xs (x,y) = maybe (Cell x y False False []) id (find (\(Cell x1 y1 _ _ _) -> x1 == x && y1 == y) xs)
 
-killIfNeighbours :: (a -> Bool) -> Cell -> Cell
+killIfNeighbours :: (Int -> Bool) -> Cell -> Cell
 killIfNeighbours condition cell@(Cell _ _ _ _ neighbouringCells) = 
-	if (condition (length (filter alive neighbouringCells))) then (setL aliveInNextTick False cell) else cell
+	if (condition (length (filter (view alive) neighbouringCells))) then (set aliveInNextTick False cell) else cell
 
 -- first collect all neighbouring cells in one place - then make individual counts of each of them - then birth the ones that were dead
 birthNewCells :: [Cell] -> [Cell]
-birthNewCells a = nub a:(getNewBirthCells a)
+birthNewCells a = nub (concat [a, (getNewBirthCells a)])
 
 getNewBirthCells :: [Cell] -> [Cell]
-getNewBirthCells = (map neighbouringCells) |> concat |> uniquesWithCounts |> (filter (snd |> (>=3))) |> map (setL aliveInNextTick True)
+getNewBirthCells = (map (view neighbouringCells)) |> concat |> uniquesWithCounts |> (filter (snd |> (>=3))) |> map (set aliveInNextTick True)
 
 finalize :: Cell -> Cell
-finalize x = setL alive (getL aliveInNextTick x) x
+finalize x = (x.alive .= x.aliveInNextTick)
 
 
 -- utils
 -- pipe operator for convenience
-(|>) f g = g . f
-
+-- (|>) f g = g . f
+uniquesWithCounts :: [[a]] -> [a]
 uniquesWithCounts = Map.fromListWith (+) . map (, 1)
 
 allCombinations (x:xs) (y:ys) = concat (x, y) (allCombinations xs ys) (allCombinations x ys) (allCombinations y xs)
