59bde9ec4c1256ad2473fb2b7a4e17009d571860
Roopesh Shenoy <shenoy.roopesh@gmail.com>
2016-06-23 02:51:16 +0530

    Some simplifications

--- gameOfLife.hs
+++ gameOfLife.hs
@@ -2,24 +2,23 @@ import Data.Map
 
 data Cell = Cell { x :: Int, y :: Int, alive :: Bool, aliveInNextTick :: Bool, neighbouringCells :: [Cell] }
 
-tick = (\x = map (fillNeighbours x) x) |> map (killIfNeighbours (<2)) |> map (killIfNeighbours (>3)) |> birthNewCells |> (map finalize)
+tick = (\xs = map (fillNeighbours xs) xs) |> map (killIfNeighbours (<2)) |> map (killIfNeighbours (>3)) |> birthNewCells |> (map finalize)
 
-fillNeighbours currentList cell@(Cell x y _ _ _) = 
-	setL neighbouringCells (map cellFromPosition (allCombinations [x-1 .. x+1] [y-1 .. y+1])) cell
+fillNeighbours xs cell@(Cell x y _ _ _) = 
+	setL neighbouringCells (map cellFromPosition xs (allCombinations [x-1 .. x+1] [y-1 .. y+1])) cell
 
-cellFromPosition currentList (x,y) = case (find (\cell -> getL x cell == x && getL y cell == y) currentList) of
-										Just cell -> cell
-										Nothing   -> (Cell x y False False [])
+-- if find cell in list then give it back, else create new cell
+cellFromPosition xs (x,y) = maybe (Cell x y False False []) id (find (\cell -> getL x cell == x && getL y cell == y) xs)
 
 killIfNeighbours condition cell@(Cell _ _ _ _ neighbouringCells) = 
 	if (condition (length (filter alive neighbouringCells)) then (setL aliveInNextTick False cell) else cell
 
 -- first collect all neighbouring cells in one place - then make individual counts of each of them - then birth the ones that were dead
-addCellsToBeBirthed a = nub  a:(getNewBirthCells a)
+birthNewCells a = nub a:(getNewBirthCells a)
 
 getNewBirthCells = (map neighbouringCells) |> concat |> uniquesWithCounts |> (filter (count |> >= 3)) |> map (setL aliveInNextTick true)
 
-finalize (Cell x y alive aliveInNextTick neighbouringCells) = (Cell x y aliveInNextTick aliveInNextTick neighbouringCells)
+finalize x = setL alive (getL aliveInNextTick x) x
 
 
 -- utils
