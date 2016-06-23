302fc93c9a955e5d10efc30a1c24460db3ee51e4
Roopesh Shenoy <shenoy.roopesh@gmail.com>
2016-06-23 19:01:59 +0530

    Fixing indent

--- gameOfLife.hs
+++ gameOfLife.hs
@@ -26,7 +26,7 @@ locnFromCell (Cell x y _ _ _) = (x, y)
 -- if find cell in list then give it back, else create new cell
 cellFromLocn :: [Cell] -> Location -> Cell
 cellFromLocn xs l@(x,y) = maybe defaultCell id $ find (locnFromCell |> (l ==)) xs 
-							where defaultCell = (Cell x y False False [])
+	where defaultCell = (Cell x y False False [])
 
 fillNeighbours :: [Cell] -> Cell -> Cell
 fillNeighbours xs (Cell x y a an _) = (Cell x y a an neighborCells)
