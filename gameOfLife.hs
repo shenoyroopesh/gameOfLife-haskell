d73ff589c201494be089a621317fdbcb612679af
Roopesh Shenoy <shenoy.roopesh@gmail.com>
2016-06-23 16:16:08 +0530

    Simplifying everything - everything need not be its own top-level function

--- gameOfLife.hs
+++ gameOfLife.hs
@@ -4,56 +4,47 @@
 module GameOfLife (tick) where
 
 import Data.List
-import Control.Lens (makeLenses, set, view)
+import Control.Lens (makeLenses, set, (^.))
 import qualified Data.Map as Map
 import Utils
 
 -- data type
-data Cell = Cell { _x :: Int, _y :: Int, _alive :: Bool, _aliveInNextTick :: Bool, _neighbouringCells :: [Cell] }
+data Cell = Cell { _x :: Int, _y :: Int, _alive :: Bool, _willBeAlive :: Bool, _neighbors :: [Cell] }
 makeLenses ''Cell
 
 instance Eq (Cell) where
 	(==) a b = posnFromCell a == posnFromCell b
 	
 instance Ord (Cell) where
-	compare (Cell x1 y1 _ _ _) (Cell x2 y2 _ _ _) = if result == EQ then compare y1 y2 else result where result = compare x1 x2
+	compare c1 c2 = if result == EQ then compare (c1^.y) (c2^.y) else result where result = compare (c1^.x) (c2^.x)
 
 posnFromCell :: Cell -> (Int, Int)
 posnFromCell (Cell x y _ _ _) = (x, y)
 
 -- if find cell in list then give it back, else create new cell
 cellFromPosition :: [Cell] -> (Int, Int) -> Cell
-cellFromPosition xs c@(x,y) = maybe (Cell x y False False []) id (find (posnFromCell |> (c ==)) xs)
+cellFromPosition xs c@(x,y) = maybe (Cell x y False False []) id $ find (posnFromCell |> (c ==)) xs
 
 fillNeighbours :: [Cell] -> Cell -> Cell
-fillNeighbours xs (Cell x y a an _) = (Cell x y a an (map (cellFromPosition xs) $ neighbouringPosns x y))
+fillNeighbours xs (Cell x y a an _) = (Cell x y a an $ map (cellFromPosition xs) $ neighborPosns)
+									where neighborPosns = delete (x,y) $ allCombinations [(x-1) .. (x+1)] [(y-1) .. (y+1)]
 
--- key external world interface												
+-- key external world interface	
 tick::[(Int, Int)] -> [(Int, Int)]
-tick = (map $ cellFromPosition []) |> tickInternal |> (map $ posnFromCell)
+tick = (map $ cellFromPosition []) |> tickInternal |> (map posnFromCell)
 
 -- all the functions we need
 tickInternal :: [Cell] -> [Cell]
-tickInternal =  (map $ set alive True |> set aliveInNextTick True) 
-			|> (\xs -> map (fillNeighbours xs) xs) 
-			|> (map $ killIfNeighbours (<2) |> killIfNeighbours (>3)) 
-			|> birthNewCells |> (map finalize) |> (filter $ view alive) |> sort
+tickInternal =  (map $ set alive True |> set willBeAlive True)
+			|> fillNeighboursForAll |> killRequiredCells |> birthNewCells |> (map updateStatus) |> (filter (^.alive)) |> sort
+			where
+				fillNeighboursForAll xs = map (fillNeighbours xs) xs
+				killRequiredCells = (map $ killIfNeighbors (<2) |> killIfNeighbors (>3))
+				killIfNeighbors condn cell = 
+					if (condn $ length $ filter (^. alive) (cell ^. neighbors)) then (set willBeAlive False cell) else cell
+				birthNewCells a = nub $ a ++ (getNewCells a)
+				updateStatus x = set alive (x^.willBeAlive) x
 
-neighbouringPosns :: Int -> Int -> [(Int, Int)]	
-neighbouringPosns x y = delete (x,y) $ allCombinations [(x-1) .. (x+1)] [(y-1) .. (y+1)]
-
-
-killIfNeighbours :: (Int -> Bool) -> Cell -> Cell
-killIfNeighbours condition cell@(Cell _ _ _ _ nc) = 
-	if (condition $ length $ filter (view alive) nc) then (set aliveInNextTick False cell) else cell
-
--- first collect all neighbouring cells in one place - then make individual counts of each of them - then birth the ones that were dead
-birthNewCells :: [Cell] -> [Cell]
-birthNewCells a = nub $ a ++ (getNewBirthCells a)
-
-getNewBirthCells :: [Cell] -> [Cell]
-getNewBirthCells = (concatMap $ view neighbouringCells) |> uniquesWithCounts |> (filter (snd |> (== 3))) 
-			|> (map $ fst |> set aliveInNextTick True)
-
-finalize :: Cell -> Cell
-finalize x = set alive (view aliveInNextTick x) x
+getNewCells :: [Cell] -> [Cell]
+getNewCells = (concatMap (^. neighbors)) |> uniquesWithCounts |> (filter (snd |> (== 3))) 
+			|> (map $ fst |> set willBeAlive True)
